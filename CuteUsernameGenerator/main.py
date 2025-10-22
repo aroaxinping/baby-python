212911139341712164413f8082215b6fdad37144
aroa <aroaxinping@gmail.com>
2025-10-22 23:28:21 +0200

    add cute username generator

---
+++ CuteUsernameGenerator/main.py
@@ -0,0 +1,14 @@
+print("Welcome to the Cute Username Generator!\nlet's get you started :)\n")
+
+#print("Hello " + input("What's your name?") + "!") estructura 1 NO USAR
+
+# username = input("What's your name?")
+# length = len(username)
+# print(length)
+# estructura 2
+
+color = input("- What's your favorite color? ")
+print("oh yes," + color + " is very pretty!\n\nNow, one last question...")
+animal = input("- What's your favorite animal? ")
+print(animal + " really are cool, I agree!\n")
+print("Well done! your new username is " + "@" + color + animal + "!\nThat's so cuuutee!")
